6d23dea38ca13373d506d9221245026bd588468d
Disha Padia <113919044+dishaapadiaa@users.noreply.github.com>
2024-07-25 17:14:46 -0400

    Added functionality

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from enum import Enum
 from typing import List, Annotated
-
-from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi import FastAPI, HTTPException, Depends, status, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+import os
+import shutil
 
 import models
 from database import SessionLocal, engine
@@ -11,18 +13,16 @@ from database import SessionLocal, engine
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:8000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+UPLOAD_DIRECTORY = "./uploaded_files"
+os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
 
 
 class UserBase(BaseModel):
@@ -37,6 +37,33 @@ class UserLogin(BaseModel):
     password: str
 
 
+class JobStatusEnum(str, Enum):
+    completed = "completed"
+    running = "running"
+    incomplete = "incomplete"
+
+
+class JobBase(BaseModel):
+    filename: str
+    description: str
+    status: JobStatusEnum
+    progress: int
+
+
+class JobCreate(JobBase):
+    pass
+
+
+class JobUpdate(BaseModel):
+    status: JobStatusEnum
+    progress: int
+
+
+class JobResponse(JobBase):
+    class Config:
+        orm_mode = True
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -46,6 +73,7 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+db_depend = Annotated[Session, Depends(get_db)]
 
 
 @app.post("/signup/", status_code=status.HTTP_201_CREATED)
@@ -104,3 +132,49 @@ async def delete_user(username: str, db: db_dependency):
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/jobs/", response_model=List[JobResponse])
+async def get_jobs(db: db_depend):
+    return db.query(models.Job).all()
+
+
+@app.put("/jobs/{filename}", response_model=JobResponse)
+async def update_job(filename: str, job: JobUpdate, db: db_depend):
+    db_job = (db.query(models.Job)
+              .filter(models.Job.filename == filename)
+              .first())
+    if db_job is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Job not found")
+    db_job.status = job.status
+    db_job.progress = job.progress
+    db.commit()
+    db.refresh(db_job)
+    return db_job
+
+
+@app.post("/jobs/", response_model=JobResponse, status_code=status.HTTP_201_CREATED)
+async def create_job(job: JobCreate, db: db_depend):
+    db_job = models.Job(**job.dict())
+    db.add(db_job)
+    db.commit()
+    db.refresh(db_job)
+    return db_job
+
+
+@app.delete("/jobs/{filename}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_job(filename: str, db: Session = Depends(get_db)):
+    db_job = db.query(models.Job).filter(models.Job.filename == filename).first()
+    if db_job is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Job not found")
+    db.delete(db_job)
+    db.commit()
+    return {"msg": "Job deleted successfully"}
+
+
+@app.post("/FileUpload/")
+async def upload_file(file: UploadFile = File(...)):
+    file_location = os.path.join(UPLOAD_DIRECTORY, file.filename)
+    with open(file_location, "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)
+    return {"info": f"file '{file.filename}' saved at '{file_location}'"}
--- models.py
+++ models.py
@@ -1,12 +1,27 @@
-from sqlalchemy import Column, String
-
+import enum
+from sqlalchemy import Column, String, Integer, Enum
 from database import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
-    username = Column(String(50), primary_key=True, unique=True, nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
-    name = Column(String(100), nullable=False)
-    password = Column(String(100), nullable=False)
+    username = Column(String, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    name = Column(String)
+    password = Column(String)
+
+
+class JobStatus(enum.Enum):
+    completed = "completed"
+    running = "running"
+    incomplete = "incomplete"
+
+
+class Job(Base):
+    __tablename__ = "jobs"
+
+    filename = Column(String(50), primary_key=True, unique=True, nullable=False)
+    description = Column(String(100), unique=True, nullable=False)
+    status = Column(Enum(JobStatus), nullable=False)
+    progress = Column(Integer, nullable=False, default=0)
